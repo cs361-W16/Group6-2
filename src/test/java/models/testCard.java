b2ec331f5e40c0980bd67da84b6d411c16d2ddf4
wangdaye123 <wangzhao@oregonstate.edu>
2016-02-13 16:31:06 -0800

    make US class and SP classes
    make US class and SP classes

--- src/test/java/models/testCard.java
+++ src/test/java/models/testCard.java
@@ -15,6 +15,16 @@ public class testCard {
     }
 
     @Test
+    public void testGetSuit1(){
+        Card c = new Card(5,Suit.Coins);
+        assertEquals(Suit.Coins,c.getSuit());
+        Card c1 = new Card(4,Suit.Cups);
+        assertEquals(Suit.Cups,c1.getSuit());
+        Card c2 = new Card(3,Suit.Swords);
+        assertEquals(Suit.Swords,c2.getSuit());
+    }
+
+    @Test
     public void testToString(){
         Card c = new Card(5,Suit.Clubs);
         assertEquals("5Clubs",c.toString());
@@ -30,6 +40,31 @@ public class testCard {
         g.move(0,2);
         assertEquals(1,g.cols.get(2).size());
         assertEquals(0,g.cols.get(0).size());
+    }
+
+    @Test
+    public void test1forUS() {
+        US s = new US();
+        s.buildDeck2();
+        s.customDeal(0,3,6,9);
+        s.remove(2);
+        assertEquals(0,s.cols.get(2).size());
+        s.move(0,2);
+        assertEquals(1,s.cols.get(2).size());
+        assertEquals(0,s.cols.get(0).size());
+
+    }
+    @Test
+    public void test2forSP() {
+        SP s1 = new SP();
+        s1.buildDeck1();
+        s1.customDeal(0,3,6,9);
+        s1.move(0,2);
+        assertEquals(1,s1.cols.get(2).size());
+        assertEquals(0,s1.cols.get(0).size());
+        s1.remove(2);
+        assertEquals(0,s1.cols.get(2).size());
+
     }
 
 
